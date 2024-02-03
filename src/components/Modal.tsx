45b5dfb408cbce8e063a2da15e6a141e9cb73c9e
Marcin Michalski <mkamichalski@gmail.com>
2024-02-03 18:37:20 +0100

    Fix: modal impovements
    Added backdrop img

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,8 @@
-import useModal from "../hooks/useModal";
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-interface Result {}
+interface Result {
+  backdrop_path: string;
+}
 
 interface ModalProps {
   result: Result;
@@ -10,14 +11,21 @@ interface ModalProps {
 }
 
 const Modal = ({ result, closeModal, title }: ModalProps) => {
- 
-    return (
+  return (
     <motion.div
-    initial={{ opacity: 0 }}
-        animate={{ opacity: 100 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: .2 }}
-      className='fixed w-[200px] h-[300px] bg-white left-[50%] top-[50%] z-50 bg-opacity-85'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 100 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+      className='fixed w-[400px] h-[300px] left-[50%] top-[50%] z-50 bg-opacity-85 filter'
+      style={{
+        background: 'rgba(0, 0, 0, .50)',
+        backgroundImage: `url(https://image.tmdb.org/t/p/w500${result.backdrop_path})`,
+        backgroundBlendMode: 'darken',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        transform: 'translate(-50%, -50%)',
+      }}
       onClick={closeModal}>
       {title}
     </motion.div>
