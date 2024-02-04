39456d28a7f4b5bb1e8217aea6a94e5dd1b220bd
Marcin Michalski <mkamichalski@gmail.com>
2024-02-04 14:02:41 +0100

    Fix: Modal responsivness
    v 0.2

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,10 +14,10 @@ const Modal = ({ result, closeModal, title }: ModalProps) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 100 }}
+      animate={{ opacity: 100}}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.2 }}
-      className='fixed w-[80%] aspect-video  max-sm:aspect-[3/4] max-[480px]:w-[90%] max-sm:max-h-[90%] left-[50%] top-[50%] shadow-xl z-50 bg-opacity-85 filter'
+      className='fixed w-[80%] aspect-video max-lg:w-[90%]  max-md:aspect-[3/4] max-md:w-[80%]   max-md:max-h-[90%] max-[480px]:w-[90%] max-[480px]:h-[80%] left-[50%] top-[50%] shadow-xl z-50 bg-opacity-85 filter'
       style={{
         background: 'rgba(0, 0, 0, .50)',
         backgroundImage: `url(https://image.tmdb.org/t/p/w1280${result.backdrop_path})`,
